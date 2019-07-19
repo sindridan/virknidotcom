711f85f7f40cdf04404a067c12d363474c0690c4
Sindri Dan <sindridan@gmail.com>
2019-07-19 01:33:47 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
+import NavBar from './Components/Navbar/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar>Good</NavBar>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>This is my paragraph. Welcome to sdg.is</p>
         <a
           className="App-link"
           href="https://reactjs.org"
