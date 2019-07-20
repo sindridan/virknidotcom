831f49df83099aac8685a3c54d0e47574010ff79
Sindri <sdgardarsson@ossur.com>
2019-07-20 03:46:32 +0000

    Styled components implemented

--- src/Components/Navbar/NavbarItems/NavbarItems.js
+++ src/Components/Navbar/NavbarItems/NavbarItems.js
@@ -1,21 +1,28 @@
 
 import React from 'react';
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import styled from 'styled-components';
+
+
+const NavItem = styled.li`
+  font-size: 30px;
+  font-color: red;
+`
 
 const NavbarItems = () => {
     return (
-        <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
+        <ul>
+            <li>
                 <Link to="/about-me" className="nav-link">About me</Link>
             </li>
-            <li className="nav-item">
+            <li >
                 <a href="https://github.com/sindridan" className="nav-link">My GitHub</a>
             </li>
-            <li className="nav-item">
+            <li >
                 <a href="https://linkedin.com/in/sindridan" className="nav-link">My LinkedIn</a>
             </li>
 
-            <li className="nav-item">
+            <li>
                 <Link to="/my-cv" className="nav-link">My CV</Link>
             </li>
         </ul>
