d6a5459683464f1045d2577f36464188ec54d412
Sindri <sdgardarsson@ossur.com>
2020-03-23 03:55:27 +0000

    fixed width for app

--- src/Components/GitHubAPIView/GitHubAPIView.js
+++ src/Components/GitHubAPIView/GitHubAPIView.js
@@ -48,16 +48,21 @@ class GitHubAPIView extends React.Component {
         return (
             <div>
                 <RepoListView list={ gitData } />
-                <PieChart
-                    data={[
-                        
-                        { title: 'One', value: 10, color: '#E38627' },
-                        { title: 'Two', value: 15, color: '#C13C37' },
-                        { title: 'Three', value: 20, color: '#6A2135' },
-                        ]}
-                />
+                
             </div>
     )}
 };
 
 export default GitHubAPIView;
+
+/*
+<PieChart
+                    data={[
+                        
+                        { title: 'One', value: 10, color: '#E38627' 
+                         },
+                        { title: 'Two', value: 15, color: '#C13C37' },
+                        { title: 'Three', value: 20, color: '#6A2135' },
+                        ]}
+                />
+*/
