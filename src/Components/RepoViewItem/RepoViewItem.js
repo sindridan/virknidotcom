2bf2a10d89f18edbc7d93ae26e4d7f46000679fe
Sindri <sdgardarsson@ossur.com>
2020-03-30 03:42:18 +0000

    cleaned the link contents

--- src/Components/RepoViewItem/RepoViewItem.js
+++ src/Components/RepoViewItem/RepoViewItem.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const RepoViewItem = (props) => {
-	console.log(props.repoItem.url)
 	return (
         <li className="card border-dark mb-3">
     		<div className="card-header">{props.repoItem.language}</div>
