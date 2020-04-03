589f5c59f700c3da26cc84f5bb3b68df42b2ad7e
Sindri <sdgardarsson@ossur.com>
2020-04-03 01:38:01 +0000

    work started on right column for git view, statistics and graphs

--- src/Components/GitHubAPIView/GitHubAPIView.js
+++ src/Components/GitHubAPIView/GitHubAPIView.js
@@ -4,11 +4,31 @@ import { sortedLangs, langsFilter } from '../../Services/GitHubAPIService'
 import PieChart from 'react-minimal-pie-chart';
 import styled from 'styled-components';
 
+const GitBodyContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-basis: 100%;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+    `
 
 const GitView = styled.div`
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
+`
 
+const ColTitle = styled.div`
+    font-size: 2em;
+    font-weight: bold;
+`
+
+const ObjectTitle = styled.div`
+    font-size: 1em;
+    
+`
+
+const TotalPieView = styled.div`
+    margin: 1em 0 1em 0;
 `
 
 class GitHubAPIView extends React.Component {
@@ -52,22 +72,22 @@ class GitHubAPIView extends React.Component {
         //console.log(this.mapLangs(langs))
         //console.log(JSON.stringify(this.mapLangs(langs))) 
         return (
-            <GitView>
-                <RepoListView id="main-repo-list" list={ gitData } />
-            </GitView>
+        
+            <GitBodyContainer>
+                <GitView>
+                    <ColTitle>My GitHub repositories</ColTitle>
+                    <RepoListView id="main-repo-list" list={ gitData } />
+                </GitView>
+
+                <GitView>
+                    <ColTitle>Repository statistics</ColTitle>
+                    <TotalPieView>
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/45%25_pie_chart.svg/600px-45%25_pie_chart.svg.png" alt="just a dummy pie chart"/>
+                    </TotalPieView>
+                </GitView>
+            </GitBodyContainer>
+            
     )}
 };
 
 export default GitHubAPIView;
-
-/*
-<PieChart
-                    data={[
-                        
-                        { title: 'One', value: 10, color: '#E38627' 
-                         },
-                        { title: 'Two', value: 15, color: '#C13C37' },
-                        { title: 'Three', value: 20, color: '#6A2135' },
-                        ]}
-                />
-*/
